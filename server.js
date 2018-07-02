7ca0fbee3f05ed72ae1ce69fdc06e745c37a76cd
root <root@vps484575.ovh.net>
2018-07-02 16:12:09 +0200

    first commit of the server in graphQL.

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(
+    `
+    type Query {
+	hello: String
+	salut: String
+    }
+    `
+);
+
+var root = {
+    hello: () => 'Hello world!',
+    salut: () => 'Salut le monde!'
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(40000, () => console.log('Now browse to localhost:4000/graphql'));
